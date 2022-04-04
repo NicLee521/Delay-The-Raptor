19d4baf545504a0a1f136aa9a55f21ba249e16c0
NicLee521 <48967043+NicLee521@users.noreply.github.com>
2022-04-03 19:56:17 -0500

    main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,10 +9,13 @@ public class MainMenu : MonoBehaviour
 {
     public VideoPlayer video;
     private GameObject menuContainer;
+    private GameObject spaceToSkip;
     // Start is called before the first frame update
     void Start(){
         video.loopPointReached += EndReached;
         menuContainer = transform.GetChild(0).gameObject;
+        spaceToSkip = transform.GetChild(1).gameObject;
+        spaceToSkip.SetActive(true);
         menuContainer.SetActive(false);
     }
 
@@ -31,10 +34,13 @@ public class MainMenu : MonoBehaviour
     {
         video.gameObject.SetActive(false);
         menuContainer.SetActive(true);
+        spaceToSkip.SetActive(false);
     }
 
     public void OnSkip(InputValue value){
         video.gameObject.SetActive(false);
         menuContainer.SetActive(true);
+        spaceToSkip.SetActive(false);
+
     }
 }
